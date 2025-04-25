5a5efc9cf1f6c0481ff02b04d127fd08d5404441
LeoNakashima <leonardo.chen@picpay.com>
2025-04-25 17:55:58 -0300

    arrumando parte de notas

--- projeto-angular/src/app/auth/notas/notas.component.ts
+++ projeto-angular/src/app/auth/notas/notas.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../layout/header/header.component';
+import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notas',
-  imports: [],
+  imports: [HeaderComponent, SidebarComponent],
   templateUrl: './notas.component.html',
   styleUrl: './notas.component.css'
 })
 export class NotasComponent {
-
+  constructor(private router: Router) {}
+  fazerCadastro() {
+    this.router.navigate(['cadastrarAluno']);
+  }
 }
